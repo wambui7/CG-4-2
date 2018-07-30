b746949ed54db638fdb2b05a7eb67a735fb2d057
wambui7 <shiko.sheila@gmail.com>
2018-07-30 18:48:36 -0500

    Commit all

--- arrays homework/Program.cs
+++ arrays homework/Program.cs
@@ -6,7 +6,15 @@ namespace arrays_homework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+
+            int[] myarrays = new int[] { 1, 1, 2, 3, 5, 8 };
+            foreach(int mynumbers in myarrays)
+            {
+                Console.WriteLine(mynumbers);
+
+            }
+            Console.ReadLine();
         }
     }
 }
